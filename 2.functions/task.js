d1d6021ab8d3876860dc81f4234fc8aaa2314425
ss13 <ssa0618@yandex.ru>
2023-06-25 14:54:18 +0300

    add 1-6 tasks

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,77 @@
+'use strict';
+
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = - Infinity;
+  let total = 0;
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i];
+    }
+    if(arr[i] > max){
+      max = arr[i];
+    }
+    total += arr[i];
+  }
+  const avg = parseFloat((total / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  return arr.reduce((acc, item) => {
+    acc = acc + item;
+    return acc;
+  }, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length === 0){
+    return 0;
+  }
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+  let evenElements = 0;
+  let oddElements = 0;
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 === 0){
+      evenElements += arr[i];
+     } else {
+      oddElements += arr[i];
+     }
+  }
+  return evenElements - oddElements;
 
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElements = 0;
+  let countEvenElements = 0;
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 === 0){
+      sumEvenElements += arr[i];
+      countEvenElements++;
+    }
+  }
+  if (countEvenElements === 0 ) {
+    return 0;
+  }
+  return sumEvenElements / countEvenElements;
 }
 
 function makeWork (arrOfArr, func) {
+  let maxResult = -Infinity;
+  for(let i = 0; i < arrOfArr.length; i++){
+    const arr = arrOfArr[i];
+    const result = func(...arr);
+    if (result > maxResult){
+      maxResult = result;
+    }
+  }
+  return maxResult;
 
 }
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  return arr2.every((item, index) => item === arr1[index]);
+
+//   if (arr1.lenght !== arr2.lenght) {
+//     return false;
+//   }
+//   return arr2.every((item, index) => item === arr1[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const usersByGender = users.filter((user) => user.gender === gender);
+  const usersAge = usersByGender.map((user) => user.age);
+  const totalAge = usersAge.reduce((acc, age) => acc + age, 0);
+  if (usersAge.length === 0) {
+    return 0;
+  }
+  return totalAge / usersAge.length;
 }
