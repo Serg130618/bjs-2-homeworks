c46de22e366002ca0e12f131d59799f43998665e
ss13 <ssa0618@yandex.ru>
2023-07-13 16:54:59 +0300

    7 and 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,41 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  const cache = [];
+  function wrapper(...args) {
+    const hash = md5(args);
+    const objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      return "Из кэша: " + objectInCache.value;
+    } else {
+      const value = func(...args);
+      cache.push({ hash, value });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      return "Вычисляем: " + value;
+    }
+  }
+  return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
-}
+  let timeoutId;
+  let isTrottled = false;
+  function wrapper(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(args);
+      wrapper.count++;
+    }, delay);
+    if (!isTrottled) {
+      func(...args);
+      wrapper.count++;
+      isTrottled = true;
+    }
+    wrapper.allCount++;
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
+}    
